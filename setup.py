2ae7bf25c9d4bec2eeb48b6022bd01155df105fd
Joshua Katz <gravypod@gravypod.com>
2016-11-22 00:21:31 -0500

    Rename to pyporktools

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@
 
 from distutils.core import setup
 
-setup(name="pyhamtools",
+setup(name="pyporktools",
 	version="0.0.1",
 	description="A python3 implementation of hamtools",
 	author="Joshua D. Katz",
 	author_email="gravypod@gravypod.com",
-	packages=["pyhamtools"],
+	packages=["pyporktools"],
 	install_requires=["cachetools", "pykml"]
 )
